0e2a9f7401fb025ad9e6c2925ace477c7f164cf8
Li Lin <guru.lin@gmail.com>
2025-07-20 22:33:34 +0800

    update prompt

--- sec_doc_tool/tagging/llm_tagger_prompt.py
+++ sec_doc_tool/tagging/llm_tagger_prompt.py
@@ -6,15 +6,15 @@ including Prospectus and Statement of Additoinal Information (a.k.a SAI).
 Please read through it and produce a 150 words summary of the content.
 In addition, label the contents with tags below:
 
-- "fund_names": a list of fund names mentioned in the snippet, separated by comma
-- "fund_tickers": a list of fund tickers mentioned in the snippet, separated by comma
-- "is_prospectus": "yes" if the snippet contains a prospectus section, otherwise "no"
-- "is_sai": "yes" if the snippet contains a sentence that indicates it is part of statement of additional information (SAI), otherwise "no"
-- "has_portfolio_manager_bio": "yes" if the snippet contains a portfolio manager biography, otherwise "no"
-- "has_portfolio_manager_ownership": "yes" if the snippet contains information about beneficial ownership by the portfolio manager ownership information, otherwise "no"
-- "has_fund_management_info": "yes" if the snippet contains information about fund management, otherwise "no"
-- "has_trustee_bio": "yes" if the snippet contains a trustee (a.k.a. independent trustee, interested person, chairman of commitee etc) biography, otherwise "no"
-- "has_trustee_compensation": "yes" if the snippet contains information about trustee compensation, otherwise "no"
+- fund_names: a list of fund names mentioned in the snippet, separated by comma
+- fund_tickers: a list of fund tickers mentioned in the snippet, separated by comma
+- is_prospectus: "yes" if the snippet contains a prospectus section, otherwise "no"
+- is_sai: "yes" if the snippet contains a sentence that indicates it is part of statement of additional information (SAI), otherwise "no"
+- has_portfolio_manager_bio: "yes" if the snippet contains a portfolio manager biography, otherwise "no"
+- has_portfolio_manager_ownership: "yes" if the snippet contains information about beneficial ownership by the portfolio manager ownership information, otherwise "no"
+- has_fund_management_info: "yes" if the snippet contains information about fund management, otherwise "no"
+- has_trustee_bio: "yes" if the snippet contains a trustee (a.k.a. independent trustee, interested person, chairman of commitee etc) biography, otherwise "no"
+- has_trustee_compensation: "yes" if the snippet contains information about trustee compensation, otherwise "no"
 
 Please add a few more tags if you find them relevant to the content of the snippet.
 
@@ -25,7 +25,7 @@ Please add a few more tags if you find them relevant to the content of the snipp
 use the below template for response, do NOT use JSON as output format
 
 Summary:
-<summary>
+<summary of max 150 words>
 
 Tags:
 - <tag1>: yes
