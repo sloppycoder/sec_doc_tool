407d491e20e81b91a52eec41639fa091e3fbf3cf
Li Lin <guru.lin@gmail.com>
2025-08-06 20:02:31 +0800

    add delete_obj_from_storage

--- sec_doc_tool/__init__.py
+++ sec_doc_tool/__init__.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from .chunking import ChunkedDocument
 from .edgar import EdgarFiling
 from .nlp_model import get_nlp_model
-from .storage import load_obj_from_storage, write_obj_to_storage
+from .storage import delete_obj_from_storage, load_obj_from_storage, write_obj_to_storage
 from .text_extractor import ExtractedText, TextExtractor
 
 __all__ = [
@@ -13,5 +13,6 @@ __all__ = [
     "ExtractedText",
     "load_obj_from_storage",
     "write_obj_to_storage",
+    "delete_obj_from_storage",
     "get_nlp_model",
 ]
